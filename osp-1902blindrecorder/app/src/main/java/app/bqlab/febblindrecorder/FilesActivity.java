da7705878d44eac4996986b0ad5bf98a8bf6d966
Sinho <choi_sinho@naver.com>
2023-05-01 19:47:06 +0900

    1

---
+++ osp-1902blindrecorder/app/src/main/java/app/bqlab/febblindrecorder/FilesActivity.java
@@ -0,0 +1,336 @@
+package app.bqlab.febblindrecorder;
+
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Environment;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class FilesActivity extends AppCompatActivity {
+
+    //variables
+    boolean clicked;
+    int focus, soundMenuEnd, soundDisable;
+    String fileDir;
+    String[] fileNames;
+    //objects
+    TextToSpeech mTTS;
+    SoundPool mSoundPool;
+    Thread speakThread;
+    //layouts
+    LinearLayout filesBody;
+    List<FileLayout> filesBodyLayouts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_files);
+        init();
+        setupTTS();
+        loadFiles();
+        resetFocus();
+        setupSoundPool();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        speakFirst();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        shutupTTS();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        shutupTTS();
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_UP:
+                clickRight();
+                return true;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                clickLeft();
+                return true;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                clickUp();
+                return true;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                clickDown();
+                return true;
+            case KeyEvent.KEYCODE_BUTTON_X:
+                clickVToggle();
+                return true;
+            case KeyEvent.KEYCODE_BUTTON_B:
+                clickXToggle();
+                return true;
+            default:
+                return true;
+        }
+    }
+
+    private void init() {
+        //initialize
+        filesBody = findViewById(R.id.folders_body);
+        filesBodyLayouts = new ArrayList<FileLayout>();
+        fileDir = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "음성메모장" + File.separator + getSharedPreferences("setting", MODE_PRIVATE).getString("SAVE_FOLDER_NAME", "");
+        //setup
+        findViewById(R.id.files_bot_up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickUp();
+            }
+        });
+        findViewById(R.id.files_bot_down).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickDown();
+            }
+        });
+        findViewById(R.id.files_bot_left).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickLeft();
+            }
+        });
+        findViewById(R.id.files_bot_right).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickRight();
+            }
+        });
+        findViewById(R.id.files_bot_enter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickVToggle();
+            }
+        });
+        findViewById(R.id.files_bot_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickXToggle();
+            }
+        });
+    }
+
+    private void clickUp() {
+        shutupTTS();
+        focus--;
+        if (focus < 0) {
+            mSoundPool.play(soundMenuEnd, 1, 1, 0, 0, 1);
+            focus = 0;
+        }
+        speakFocus();
+        resetFocus();
+    }
+
+    private void clickDown() {
+        shutupTTS();
+        focus++;
+        if (focus > filesBodyLayouts.size() - 1) {
+            mSoundPool.play(soundMenuEnd, 1, 1, 0, 0, 1);
+            focus = filesBodyLayouts.size() - 1;
+        }
+        speakFocus();
+        resetFocus();
+    }
+
+    private void clickLeft() {
+        shutupTTS();
+        startActivity(new Intent(FilesActivity.this, SearchActivity.class));
+        finish();
+    }
+
+    private void clickRight() {
+        shutupTTS();
+        String fileName = fileNames[focus];
+        if (new File(fileDir, fileName).exists()) {
+            File file = new File(fileDir, fileName);
+            Intent i = new Intent(FilesActivity.this, PlayActivity.class);
+            i.putExtra("filePath", file.getPath());
+            i.putExtra("flag", "list");
+            startActivity(i);
+        } else {
+            loadFiles();
+        }
+    }
+
+    private void clickVToggle() {
+        mSoundPool.play(soundDisable, 1, 1, 0, 0, 1);
+    }
+
+    private void clickXToggle() {
+        shutupTTS();
+        if (clicked) {
+            //두번째 클릭
+            try {
+                File file = new File(fileDir, fileNames[focus]);
+                boolean success = file.delete();
+                loadFiles();
+                resetFocus();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            //첫번째 클릭
+            clicked = true;
+            speakThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    speak("한번 더 누르면 파일이 삭제됩니다.");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            new CountDownTimer(6000, 1000) { //딜레이 동안 한번 더 토글 클릭 입력시 파일 삭제
+                                @Override
+                                public void onTick(long millisUntilFinished) {
+
+                                }
+
+                                @Override
+                                public void onFinish() {
+                                    clicked = false;
+                                }
+                            }.start();
+                        }
+                    });
+                }
+            });
+            speakThread.start();
+        }
+    }
+
+    private void loadFiles() {
+        if (new File(fileDir).list().length == 0) {
+            startActivity(new Intent(this, MainActivity.class));
+            speakThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    speak("저장된 파일이 없습니다.");
+                }
+            });
+            speakThread.start();
+        } else {
+            //파일을 커스텀 레이아웃인 FileLayout으로 치환하여 뷰그룹에 추가(파일 리스트->레이아웃 그룹으로 변환 정도로 이해하면 쉽습니다)
+            filesBody.removeAllViews();
+            File dir = new File(fileDir);
+            fileNames = dir.list();
+            filesBodyLayouts = new ArrayList<>();
+            if (fileNames.length != 0) {
+                for (int i = 0; i < fileNames.length; i++) {
+                    FileLayout fileLayout = new FileLayout(this, String.valueOf(i + 1), fileNames[i]);
+                    filesBodyLayouts.add(fileLayout);
+                    filesBody.addView(fileLayout);
+                }
+            }
+        }
+    }
+
+    private void resetFocus() {
+        for (int i = 0; i < filesBodyLayouts.size(); i++) {
+            if (i != focus) {
+                //포커스가 없는 버튼 처리
+                filesBodyLayouts.get(i).setColor(getDrawable(R.drawable.app_button));
+            } else {
+                //포커스를 가진 버튼 처리
+                filesBodyLayouts.get(i).setColor(getDrawable(R.drawable.app_button_focussed));
+            }
+        }
+    }
+
+    private void setupSoundPool() {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        mSoundPool = new SoundPool.Builder()
+                .setMaxStreams(2)
+                .setAudioAttributes(audioAttributes)
+                .build();
+        soundMenuEnd = mSoundPool.load(this, R.raw.app_sound_menu_end, 0);
+        soundDisable = mSoundPool.load(this, R.raw.app_sound_disable, 0);
+    }
+
+    private void setupTTS() {
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.KOREA);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(FilesActivity.this, "지원하지 않는 서비스입니다.", Toast.LENGTH_LONG).show();
+                        finishAffinity();
+                    }
+                } else {
+                    Toast.makeText(FilesActivity.this, "지원하지 않는 서비스입니다.", Toast.LENGTH_LONG).show();
+                    finishAffinity();
+                }
+            }
+        });
+        mTTS.setPitch(0.7f);
+        mTTS.setSpeechRate(1.2f);
+    }
+
+    private void shutupTTS() {
+        try {
+            speakThread.interrupt();
+            speakThread = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void speak(String text) {
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    private void speakFirst() {
+        speakThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(500);
+                    speak("현재 폴더는 " + getSharedPreferences("setting", MODE_PRIVATE).getString("SAVE_FOLDER_NAME", ""));
+                    Thread.sleep(3000);
+                    speak("파일목록");
+                    Thread.sleep(1500);
+                    speakFocus();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        speakThread.start();;
+    }
+
+    private void speakFocus() {
+        final Button button = filesBodyLayouts.get(focus).getButton();
+        speakThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                speak(String.valueOf(focus + 1) + "번파일 " + button.getText().toString().replace(".mp4", ""));
+            }
+        });
+        speakThread.start();
+    }
+}
---
+++ osp-1902blindrecorder/app/src/main/java/app/bqlab/febblindrecorder/PlayActivity.java
@@ -0,0 +1,333 @@
+package app.bqlab.febblindrecorder;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.os.Environment;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class PlayActivity extends AppCompatActivity {
+
+    //variables
+    int soundDisable;
+    boolean playing, speaking;
+    String fileName, fileDir, filePath;
+    //objects
+    File mFile;
+    TextToSpeech mTTS;
+    MediaPlayer mPlayer;
+    MediaRecorder mRecorder;
+    HashMap<String, String> mTTSMap;
+    SoundPool mSoundPool;
+    Thread speakThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        init();
+        setupSoundPool();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setupTTS();
+        speakFirst();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopPlaying();
+        shutupTTS();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopPlaying();
+        shutupTTS();
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_UP:
+                clickRight();
+                return true;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                clickLeft();
+                return true;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                clickUp();
+                return true;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                clickDown();
+                return true;
+            case KeyEvent.KEYCODE_BUTTON_X:
+                clickVToggle();
+                return true;
+            case KeyEvent.KEYCODE_BUTTON_B:
+                clickXToggle();
+                return true;
+            default:
+                return true;
+        }
+    }
+
+    private void init() {
+        //initialize
+        filePath = getIntent().getStringExtra("filePath");
+        fileDir = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "음성메모장" + File.separator + getSharedPreferences("setting", MODE_PRIVATE).getString("SAVE_FOLDER_NAME", "");
+        fileName = filePath.replace(fileDir + File.separator, "");
+        mFile = new File(fileDir, fileName);
+        //setup
+        findViewById(R.id.play_bot_up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickUp();
+            }
+        });
+        findViewById(R.id.play_bot_down).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickDown();
+            }
+        });
+        findViewById(R.id.play_bot_left).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickLeft();
+            }
+        });
+        findViewById(R.id.play_bot_right).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickRight();
+            }
+        });
+        findViewById(R.id.play_bot_enter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickVToggle();
+            }
+        });
+        findViewById(R.id.play_bot_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickXToggle();
+            }
+        });
+    }
+
+    private void clickUp() {
+        mSoundPool.play(soundDisable, 1, 1, 0, 0, 1);
+    }
+
+    private void clickDown() {
+        mSoundPool.play(soundDisable, 1, 1, 0, 0, 1);
+    }
+
+    private void clickLeft() {
+        shutupTTS();
+        String flag = getIntent().getStringExtra("flag");
+        if (flag.equals("list")) {
+            startActivity(new Intent(PlayActivity.this, FilesActivity.class));
+            finish();
+        } else if (flag.equals("name")) {
+            startActivity(new Intent(PlayActivity.this, SearchActivity.class));
+            finish();
+        }
+    }
+
+    private void clickRight() {
+        mSoundPool.play(soundDisable, 1, 1, 0, 0, 1);
+    }
+
+    private void clickVToggle() {
+        shutupTTS();
+        if (playing)
+            stopPlaying();
+        else
+            startPlaying();
+    }
+
+    private void clickXToggle() {
+        mSoundPool.play(soundDisable, 1, 1, 0, 0, 1);
+    }
+
+    private void setupSoundPool() {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+        mSoundPool = new SoundPool.Builder()
+                .setMaxStreams(1)
+                .setAudioAttributes(audioAttributes)
+                .build();
+        soundDisable = mSoundPool.load(this, R.raw.app_sound_disable, 0);
+    }
+
+    private void setupTTS() {
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.KOREA);
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(PlayActivity.this, "지원하지 않는 서비스입니다.", Toast.LENGTH_LONG).show();
+                        finishAffinity();
+                    }
+                } else {
+                    Toast.makeText(PlayActivity.this, "지원하지 않는 서비스입니다.", Toast.LENGTH_LONG).show();
+                    finishAffinity();
+                }
+            }
+        });
+        mTTS.setPitch(0.7f);
+        mTTS.setSpeechRate(1.2f);
+    }
+
+    private void shutupTTS() {
+        try {
+            speakThread.interrupt();
+            speakThread = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void speak(String text) {
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, mTTSMap);
+    }
+
+    private void speakFirst() {
+        speakThread = new Thread(new Runnable() {
+            @SuppressLint("SimpleDateFormat")
+            @Override
+            public void run() {
+                    final String speak = "현재 폴더는 " + getSharedPreferences("setting", MODE_PRIVATE).getString("SAVE_FOLDER_NAME", "");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //STT로 검색하여 이 화면에 도달하였을 경우 추가적으로 음성 출력
+                    if (getIntent().getStringExtra("searchResult") != null) {
+                        speakThread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                speak(speak);
+                                try {
+                                    Thread.sleep(3000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                speak("파일을 찾았습니다. 곧 재생됩니다.");
+                                try {
+                                    Thread.sleep(3500);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                        speakThread.start();
+                    }
+                    //파일 정보 음성으로 출력
+                    speakThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            speak(fileName.replace(".mp4", "") + new SimpleDateFormat(" yyyy년 MM월 dd일").format(mFile.lastModified()));
+                            try {
+                                Thread.sleep(3000);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        startPlaying();
+                                    }
+                                });
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    speakThread.start();
+            }
+        });
+        speakThread.start();
+    }
+
+    private void startPlaying() {
+        if (!playing) {
+            //MediaRecorder 속성 세팅 (확장자, 코덱 등)
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            mRecorder.setOutputFile(filePath);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            //녹음파일 MediaPlayer를 활용하여 재생
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1000);
+                        playing = true;
+                        mPlayer = new MediaPlayer();
+                        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                            @Override
+                            public void onCompletion(MediaPlayer mp) {
+                                stopPlaying();
+                                String flag = getIntent().getStringExtra("flag");
+                                if (flag.equals("list")) {
+                                    startActivity(new Intent(PlayActivity.this, FilesActivity.class));
+                                    finish();
+                                } else if (flag.equals("name")) {
+                                    startActivity(new Intent(PlayActivity.this, SearchActivity.class));
+                                    finish();
+                                }
+                                finish();
+                            }
+                        });
+                        mPlayer.setDataSource(filePath);
+                        mPlayer.prepare();
+                        mPlayer.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+            findViewById(R.id.play_button).setBackground(getDrawable(R.drawable.play_button_stop));
+        } else
+            Toast.makeText(this, "이미 파일을 재생하는 중입니다.", Toast.LENGTH_LONG).show();
+    }
+
+    private void stopPlaying() {
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+        if (mPlayer != null) {
+            mPlayer.stop();
+            mPlayer = null;
+        }
+        playing = false;
+        findViewById(R.id.play_button).setBackground(getDrawable(R.drawable.play_button_play));
+    }
+}
